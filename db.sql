0cdece9939f41e52539965c58a6eacb5d6ee0038
Joao Pedro Araujo Carlimbante <128616698+jparaujo2@users.noreply.github.com>
2024-03-08 13:56:13 -0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,10 +1,8 @@
--- Criação do banco de dados
+
 CREATE DATABASE cadastro_contatos;
 
--- Seleciona o banco de dados recém-criado
 USE cadastro_contatos;
 
--- Criação da tabela de contatos
 CREATE TABLE contatos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
